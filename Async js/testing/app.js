07ea9e34b4153563de41fa80f73df5cbfe654bfd
Nimesh Ranatunge <2018cs131@stu.ucsc.cmb.ac.lk>
2022-07-05 18:00:00 +0530

    x

--- Async js/testing/app.js
+++ Async js/testing/app.js
@@ -1,48 +1,7 @@
-const posts = [
-    {title: 'Post One', body: 'This is post one'},
-    {title: 'Post Two', body: 'This is post two'}
-  ];
-  
-  function createPost(post) {
-    setTimeout(function() {
-      posts.push(post);
-    }, 2000);
-  }
-  
-  
-  function getPosts() {
-    setTimeout(function() {
-      let output = '';
-      posts.forEach(function(post){
-        output += `<li>${post.title}</li>`;
-      });
-      document.body.innerHTML = output;
-    }, 1000);
-  }
-  
+function x(){
+  setTimeout(() => {console.log("this is the first message")}, 5000);
+  setTimeout(() => {console.log("this is the second message")}, 3000);
+  setTimeout(() => {console.log("this is the third message")}, 1000);
+}
 
-
-  createPost({title: 'Post Three', body: 'This is post three'});
-  
-  getPosts();
-  
-  
-//   function createPost(post, callback) {
-//     setTimeout(function() {
-//       posts.push(post);
-//       callback();
-//     }, 2000);
-//   }
-  
-  
-//   function getPosts() {
-//     setTimeout(function() {
-//       let output = '';
-//       posts.forEach(function(post){
-//         output += `<li>${post.title}</li>`;
-//       });
-//       document.body.innerHTML = output;
-//     }, 1000);
-//   }
-  
-//   createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
+x();
